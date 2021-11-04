255be70153a9a5a42409888ce8d11d3bb372d504
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-11-05 01:21:53 +0700

    change header background color to primary and title color to white

--- src/routes/AppRoute.tsx
+++ src/routes/AppRoute.tsx
@@ -6,12 +6,17 @@ import {
 } from '../screens';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {HomeTab} from './HomeTab';
+import {colors} from '../styles/colors';
 
 const {Navigator, Screen} = createNativeStackNavigator<any>();
 
 const AppRoute: React.FC<any> = ({}) => {
   return (
-    <Navigator>
+    <Navigator
+      screenOptions={{
+        headerStyle: {backgroundColor: colors.primary},
+        headerTitleStyle: {color: colors.white},
+      }}>
       <Screen name="Jala Media" component={HomeTab} />
       <Screen name="Detail Harga Udang" component={HargaUdangDetailScreen} />
       <Screen name="Detail Kabar Udang" component={KabarUdangDetailScreen} />
