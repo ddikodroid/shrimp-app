66eb4ad8adf2bede1e1102f8af034c37ca537d8d
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-11-05 01:14:30 +0700

    create index of screens

---
+++ src/screens/index.ts
@@ -0,0 +1,15 @@
+export {HargaUdangScreen} from './HargaUdangScreen';
+export {HargaUdangDetailScreen} from './HargaUdangDetailScreen';
+export {KabarUdangScreen} from './KabarUdangScreen';
+export {KabarUdangDetailScreen} from './KabarUdangDetailScreen';
+export {DaftarPenyakitScreen} from './DaftarPenyakitScreen';
+export {InfoPenyakitScreen} from './InfoPenyakitScreen';
+
+export const SCREENS = {
+  HARGA_UDANG: 'Harga Udang',
+  HARGA_UDANG_DETAIL: 'Detail Harga',
+  KABAR_UDANG: 'Kabar Udang',
+  KABAR_UDANG_DETAIL: 'Detail Kabar',
+  DAFTAR_PENYAKIT: 'Penyakit',
+  INFO_PENYAKIT: 'Info Penyakit',
+};
