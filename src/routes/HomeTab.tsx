37695799a7c2babbf0fef80ef2e0623268324fc9
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-11-05 01:21:25 +0700

    change tab bar indicator background color and label style to non-uppercase

--- src/routes/HomeTab.tsx
+++ src/routes/HomeTab.tsx
@@ -6,13 +6,18 @@ import {
   SCREENS,
 } from '../screens';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import {colors} from '../styles/colors';
 export type IHomeTabProps = {};
 
 const {Navigator, Screen} = createMaterialTopTabNavigator();
 
 const HomeTab: React.FC<any> = ({}) => {
   return (
-    <Navigator>
+    <Navigator
+      screenOptions={{
+        tabBarIndicatorStyle: {backgroundColor: colors.primary},
+        tabBarLabelStyle: {textTransform: 'none'},
+      }}>
       <Screen name={SCREENS.HARGA_UDANG} component={HargaUdangScreen} />
       <Screen name={SCREENS.KABAR_UDANG} component={KabarUdangScreen} />
       <Screen name={SCREENS.DAFTAR_PENYAKIT} component={DaftarPenyakitScreen} />
