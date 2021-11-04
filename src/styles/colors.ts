80a9385bfc2185b4f63fb636d15582ef29419453
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-11-05 01:13:05 +0700

    create color palette

---
+++ src/styles/colors.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  primary: '#1b77df',
+  yellow: '#fcc531',
+  white: '#ffffff',
+  dark: {
+    blue: '#004492',
+    grey: '#454646',
+    white: '#e5e5e5',
+  },
+  light: {
+    yellow: '#fff8e7',
+    grey: '#a09e9e',
+  },
+};
