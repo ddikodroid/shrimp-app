9227dc4c830109891e2d311cc48a85a517c517dd
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-11-06 00:49:16 +0700

    create typography styles

---
+++ src/styles/typography.ts
@@ -0,0 +1,14 @@
+export const fonts = {
+  black: {
+    fontFamily: 'Lato-Black',
+  },
+  bold: {
+    fontFamily: 'Lato-Bold',
+  },
+  regular: {
+    fontFamily: 'Lato-Regular',
+  },
+  light: {
+    fontFamily: 'Lato-Light',
+  },
+};
