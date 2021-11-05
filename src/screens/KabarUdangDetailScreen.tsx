87c4235322dae2530447d415d4d7a05d934f8363
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-11-06 02:21:34 +0700

    add webview component to kabar udang detail screen (done)

--- src/screens/KabarUdangDetailScreen.tsx
+++ src/screens/KabarUdangDetailScreen.tsx
@@ -1,14 +1,73 @@
-import React from 'react';
-import {Text, SafeAreaView} from 'react-native';
+import React, {useLayoutEffect} from 'react';
+import {ActivityIndicator, Share, StyleSheet, View} from 'react-native';
+import {BASE_URL} from 'react-native-dotenv';
+import Icon from 'react-native-dynamic-vector-icons';
+import WebView from 'react-native-webview';
+import {normalize} from '../helpers';
+import {colors} from '../styles';
 
-export type IKabarUdangDetailScreenProps = {};
+export type IKabarUdangDetailScreenProps = {
+  navigation: any;
+  route: any;
+};
 
-const KabarUdangDetailScreen: React.FC<IKabarUdangDetailScreenProps> = ({}) => {
+const KabarUdangDetailScreen: React.FC<IKabarUdangDetailScreenProps> = ({
+  navigation,
+  route,
+}) => {
+  const {id} = route.params;
+  const uri = `${BASE_URL}/web_view/posts/${id}`;
+
+  const handleShareButton = () => {
+    Share.share({
+      message: `${BASE_URL}/posts/${id}`,
+    });
+  };
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <Icon
+          type="Ionicons"
+          name="share-social-outline"
+          size={normalize(19)}
+          color={colors.white}
+          onPress={handleShareButton}
+        />
+      ),
+    });
+  });
+
+  const renderLoading = () => (
+    <View style={styles.loadingContainer}>
+      <ActivityIndicator size="large" color={colors.primary} />
+    </View>
+  );
   return (
-    <SafeAreaView>
-      <Text>Kabar Udang Detail</Text>
-    </SafeAreaView>
+    <WebView
+      source={{uri}}
+      containerStyle={styles.container}
+      showsVerticalScrollIndicator={false}
+      renderLoading={renderLoading}
+    />
   );
 };
 
 export {KabarUdangDetailScreen};
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 0,
+    margin: normalize(16),
+    borderRadius: normalize(8),
+  },
+  loadingContainer: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
