b8977f2af62154e4e16249460b97f6394e9fbfbb
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-11-06 13:54:02 +0700

    create kabar model

---
+++ src/store/models/kabar.ts
@@ -0,0 +1,84 @@
+import axios from 'axios';
+import {Action, Thunk, action, thunk} from 'easy-peasy';
+import {API_TOKEN, BASE_URL} from 'react-native-dotenv';
+
+interface IKabar {
+  id: number;
+  full_name: string;
+  short_name?: string;
+  image: string;
+  slug?: string;
+  meta_description: string;
+  meta_keywords?: string;
+  status?: string;
+  indication?: string;
+  pathogen?: string;
+  effect?: string;
+  stability_viability?: string;
+  handling?: string;
+  regulation?: string;
+  reference?: string;
+  created_by?: number;
+  updated_by?: number;
+  created_at: string;
+  updated_at?: string;
+}
+
+export interface IStatus {
+  fetching: boolean;
+  error: null | undefined | string;
+  success: boolean;
+}
+
+export interface IKabarModel {
+  data: IKabar[];
+  status: IStatus;
+  page: number;
+  setError: Action<IStatus, any>;
+  setFetching: Action<IStatus, any>;
+  setSuccess: Action<IStatus, any>;
+  addKabar: Action<IKabarModel, any>;
+  getKabar: Thunk<IKabarModel>;
+}
+
+export const kabar: IKabarModel = {
+  data: [],
+  status: {
+    fetching: false,
+    error: null,
+    success: false,
+  },
+  page: 1,
+  setError: action((state, payload) => {
+    state.error = payload;
+  }),
+  setFetching: action((state, payload) => {
+    state.fetching = payload;
+  }),
+  setSuccess: action((state, payload) => {
+    state.success = payload;
+  }),
+  addKabar: action((state, payload) => {
+    state.data = payload;
+  }),
+  getKabar: thunk(async (actions, payload) => {
+    try {
+      actions.setFetching(true);
+      const response = await axios.get(
+        `${BASE_URL}/api/posts?per_page=15?page=1`,
+        {
+          headers: {
+            Authorization: `Bearer ${API_TOKEN}`,
+          },
+        },
+      );
+      actions.addKabar(response.data.data);
+    } catch (error) {
+      console.log(error);
+      actions.setError(error);
+    } finally {
+      actions.setFetching(false);
+      actions.setSuccess(true);
+    }
+  }),
+};
