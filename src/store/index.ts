ef28dc24865861b81dd819ba70928b7e62685319
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-11-06 13:54:09 +0700

    add kabar model to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,15 @@
+import {IKabarModel, kabar} from './models/kabar';
 import {IPenyakitModel, penyakit} from './models/penyakit';
 import {createStore, createTypedHooks} from 'easy-peasy';
 
 export interface IStore {
   penyakit: IPenyakitModel;
+  kabar: IKabarModel;
 }
 
 const model = {
   penyakit,
+  kabar,
 };
 
 export const store = createStore<IStore>(model);
