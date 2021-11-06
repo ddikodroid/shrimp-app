a311ccc41f0b59c907a02b705641d05c8ec8451e
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-11-06 13:41:28 +0700

    wrap app using store provider

--- App.tsx
+++ App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {AppRoute} from './src/routes';
+import {StoreProvider} from 'easy-peasy';
+import {store} from './src/store';
 
 export type IAppProps = {};
 
 const App: React.FC<IAppProps> = ({}) => {
   return (
-    <NavigationContainer>
-      <AppRoute />
-    </NavigationContainer>
+    <StoreProvider store={store}>
+      <NavigationContainer>
+        <AppRoute />
+      </NavigationContainer>
+    </StoreProvider>
   );
 };
 
