6f969c16a612be65db549afdeb74e136bcdbd5c1
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-11-06 13:54:30 +0700

    get kabar udang data from api

--- src/screens/KabarUdangScreen.tsx
+++ src/screens/KabarUdangScreen.tsx
@@ -1,62 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView, FlatList, StyleSheet} from 'react-native';
 import {Gap} from '../components/Gap';
 import {KabarUdangCard} from '../components/KabarUdangCard';
 import {SectionTitle} from '../components/SectionTitle';
 import {normalize} from '../helpers';
+import {useStoreActions, useStoreState} from '../store';
 import {colors} from '../styles';
 
 export type IKabarUdangScreenProps = {};
 
-const kabarUdang = [
-  {
-    id: 124,
-    author_id: 13802,
-    category_id: 1,
-    image: 'posts/November2021/IFUtY4gc4oAHGM6cMdPX.jpg',
-    status: 'PUBLISHED',
-    featured: false,
-    advertisement: null,
-    created_at: '2021-11-05 07:30:00',
-    updated_at: '2021-11-05 07:50:13',
-    title: 'Mengenal BGA, Kelompok Plankton yang Wajib Diwaspadai',
-    seo_title: 'Kenali dan Kontrol BGA di Tambak',
-    excerpt:
-      'BGA memiliki beberapa potensi mengganggu jalannya budidaya saat terjadi ledakan populasi. Bagaimana cara menanggulanginya?',
-  },
-  {
-    id: 123,
-    author_id: 13802,
-    category_id: 1,
-    image: 'posts/November2021/IFUtY4gc4oAHGM6cMdPX.jpg',
-    status: 'PUBLISHED',
-    featured: false,
-    advertisement: null,
-    created_at: '2021-11-05 07:30:00',
-    updated_at: '2021-11-05 07:50:13',
-    title: 'Mengenal BGA, Kelompok Plankton yang Wajib Diwaspadai',
-    seo_title: 'Kenali dan Kontrol BGA di Tambak',
-    excerpt:
-      'BGA memiliki beberapa potensi mengganggu jalannya budidaya saat terjadi ledakan populasi. Bagaimana cara menanggulanginya?',
-  },
-  {
-    id: 122,
-    author_id: 13802,
-    category_id: 1,
-    image: 'posts/November2021/IFUtY4gc4oAHGM6cMdPX.jpg',
-    status: 'PUBLISHED',
-    featured: false,
-    advertisement: null,
-    created_at: '2021-11-05 07:30:00',
-    updated_at: '2021-11-05 07:50:13',
-    title: 'Mengenal BGA, Kelompok Plankton yang Wajib Diwaspadai',
-    seo_title: 'Kenali dan Kontrol BGA di Tambak',
-    excerpt:
-      'BGA memiliki beberapa potensi mengganggu jalannya budidaya saat terjadi ledakan populasi. Bagaimana cara menanggulanginya?',
-  },
-];
-
 const KabarUdangScreen: React.FC<IKabarUdangScreenProps> = ({}) => {
+  const kabar = useStoreState(state => state.kabar.data);
+  const getKabar = useStoreActions(actions => actions.kabar.getKabar);
+
+  useEffect(() => {
+    getKabar();
+  }, []);
+
   const renderHeader = () => (
     <>
       <SectionTitle title="Kabar Udang" />
@@ -69,7 +29,7 @@ const KabarUdangScreen: React.FC<IKabarUdangScreenProps> = ({}) => {
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
-        data={kabarUdang}
+        data={kabar}
         renderItem={renderKabarUdang}
         ListHeaderComponent={renderHeader}
         contentContainerStyle={styles.contentContainer}
