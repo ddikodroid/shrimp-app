f559ca620b5dbebb04fb606275132b4b2d91435c
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-11-08 00:34:37 +0700

    downgrade react-native-reanimated to 2.2.3 because of bug on react-navigation

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,13 @@
-module.exports = function (api) {
-  api.cache(false);
-  return {
-    presets: ['module:metro-react-native-babel-preset'],
-    plugins: [
-      [
-        'module:react-native-dotenv',
-        {
-          moduleName: 'react-native-dotenv',
-          path: '.env',
-        },
-      ],
-      'react-native-reanimated/plugin',
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: 'react-native-dotenv',
+        path: '.env',
+      },
     ],
-  };
+    'react-native-reanimated/plugin',
+  ],
 };
