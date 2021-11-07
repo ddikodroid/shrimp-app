1295f00bf40e8affdb316c390b4e16b1bedaeb88
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-11-08 00:20:05 +0700

    check route params (wip)

--- src/screens/HargaUdangDetailScreen.tsx
+++ src/screens/HargaUdangDetailScreen.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import {Text, SafeAreaView} from 'react-native';
 
-export type IHargaUdangDetailScreenProps = {};
+export type IHargaUdangDetailScreenProps = {
+  route: any;
+};
 
-const HargaUdangDetailScreen: React.FC<IHargaUdangDetailScreenProps> = ({}) => {
+const HargaUdangDetailScreen: React.FC<IHargaUdangDetailScreenProps> = ({
+  route,
+}) => {
+  const {item} = route.params;
   return (
     <SafeAreaView>
-      <Text>Harga Udang Detail</Text>
+      <Text style={{color: 'red'}}>{item.id}</Text>
     </SafeAreaView>
   );
 };
