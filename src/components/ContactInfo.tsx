98793f4642cbd37c57648b9f7b6345d29260a0f1
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-11-08 22:51:56 +0700

    create contact info component

---
+++ src/components/ContactInfo.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {normalize} from '../helpers';
+import {colors, fonts} from '../styles';
+import {Button} from './Button';
+
+export type IContactInfoProps = {
+  contact: string;
+};
+
+const ContactInfo: React.FC<IContactInfoProps> = ({contact}) => {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.phoneTitle}>Kontak</Text>
+        <Text style={styles.phoneText}>{contact}</Text>
+      </View>
+      <Button title="Hubungi" buttonStyle={styles.callButton} />
+    </View>
+  );
+};
+
+export {ContactInfo};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  phoneTitle: {
+    ...fonts.regular,
+    fontSize: normalize(12),
+    lineHeight: normalize(16),
+    color: colors.dark.grey,
+  },
+  phoneText: {
+    ...fonts.bold,
+    fontSize: normalize(16),
+    lineHeight: normalize(24),
+    color: colors.dark.darkerGrey,
+  },
+  callButton: {
+    flex: 0.4,
+    paddingVertical: normalize(10),
+    paddingHorizontal: normalize(10),
+  },
+});
